a1890017724de5866e376a9766776ff34f537315
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-07-13 09:45:44 +0000

    Add arithmetic script in future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = int(input("Enter your current age: "))
+
+# Calculate future 2050
+age_2050 = current_age + 27
+
+# Print the result
+print(f"In 2050, you will be {age_2050} years old.")
